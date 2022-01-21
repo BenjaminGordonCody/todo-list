2b19b5c8b94ab0b00176765fb76e6fb8a277d552
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-01-21 15:07:49 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import "./App.css";
 import React from "react";
-
-// These are all the components we've made this week
-// import Navbar from "./components/Navbar";
-// import Jumbotron from "./components/Jumbotron";
-// import Footer from "./components/Footer";
-// import Carousel from "./components/Carousel";
-// import Content from "./components/Content";
-// import CountPanel from "./components/CountingButtons";
-// import Calculator from "./components/Calculator";
 import ToDo from "./components/ToDo";
 
 function App() {
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,6 +5,7 @@ const ToDo = (args) => {
   // defining and iteracting with states --------------------------------
   const [input, setInput] = useState(""); //manages input box
   const [tasks, setTasks] = useState([]); //array of task objects
+  const [archive, setArchive] = useState([]); //this is where deleted tasks go
 
   // Task object and related funcs--------------------------------------
   class Task {
@@ -44,37 +45,68 @@ const ToDo = (args) => {
   const getTaskComponentFromObject = (task) => {
     const className = task.completed ? "complete" : "incomplete";
     return (
-      <div
-        className={className + " task"}
-        onClick={onClickTask}
-        key={task.task}
-      >
-        {task.task}
+      <div className={className + " task"}>
+        <div className="taskText" onClick={onClickTask} key={task.task}>
+          {task.task}
+        </div>
+
+        <div
+          className="delete"
+          onClick={() => {
+            onClickDelete(task.task);
+          }}
+        ></div>
       </div>
     );
   };
 
+  const getPFromObject = (task) => {
+    return <p>{task.task}</p>;
+  };
+
   const onClickTask = (event) => {
-    console.log("you clicked");
     const taskText = event.target.innerHTML;
     const taskLocation = findTaskObjectInArray(tasks, taskText);
     const tempTasks = [...tasks];
     tempTasks[taskLocation].changeTaskCompletion();
     setTasks(tempTasks);
-    console.log(tasks);
+  };
+
+  const onClickDelete = (string) => {
+    const taskText = string;
+    const taskLocation = findTaskObjectInArray(tasks, taskText);
+
+    //archive deleted task
+    const tempArchive = [...archive];
+    tempArchive.push(tasks[taskLocation]);
+    setArchive(tempArchive);
+
+    //delete task from main list
+    const tempTasks = [...tasks];
+    tempTasks.splice(taskLocation, 1);
+    setTasks(tempTasks);
   };
 
   //return ---------------------------------------------------
   return (
-    <div>
+    <div id="container">
       <form onSubmit={handleSubmission}>
         <label>
           New Task:
-          <input type="text" value={input} onChange={handleInputChange} />
+          <input
+            id="textInput"
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+          />
         </label>
-        <input type="submit" value="+" />
+        <input id="submitButton" type="submit" value="+" />
       </form>
       <div id="tasks">{tasks.map(getTaskComponentFromObject)}</div>
+      <div id="archive">
+        <h1>Archived</h1>
+        {archive.map(getPFromObject)}
+      </div>
     </div>
   );
 };
